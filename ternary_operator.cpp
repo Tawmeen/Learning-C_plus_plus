1acc2a0abebb1dd39cbffce1a2efff3e0f31507d
Tawmeen Hasan Hasib <164780505+Tawmeen@users.noreply.github.com>
2024-10-23 23:12:23 +0600

    first commit

---
+++ ternary_operator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //ternary operator ?: = replacement to an if/else condition
+    // condition ? expration 1: expration2;
+
+   // int grade = 75;
+   // grade >= 60? cout << "you pass!": cout << "you fail!";
+
+   bool hungry = false;
+
+   hungry ? cout << "you are hungray": cout << "you are full!";
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
