0f6e809c2863ed8efd3b3c0fd4cce3aefa90d88e
Tawmeen Hasan Hasib <tawmeenhasanalhasib259@gmail.com>
2024-11-06 05:21:57 +0600

    To run this code..

---
+++ while_loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+   // string name;
+   // while(name.empty()){
+     //   cout <<"Enter your name: ";
+     //   getline(cin,name);
+   // }
+   // cout<<"Hello " << name;
+   string name;
+   while(1==1){
+    cout<<"HELP!I'M STUCK IN AN INFINITE LOOP";
+   }
+
+//Ei Lopp ta shera.Try it!
+
+
+    return 0;
+
+}
