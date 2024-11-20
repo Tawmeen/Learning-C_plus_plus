95dd55fff87eddb4dc580ac517346c75f2b8aee9
Tawmeen Hasan Hasib <tawmeenhasanalhasib259@gmail.com>
2024-11-21 00:32:55 +0600

    To run this code...

---
+++ break_continue_key.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+//Break = break out of a loop.
+//Continue = skip current interation.
+
+for(int i=1; i<=20; i++){
+    if(i=23){
+        continue;
+    }
+
+        cout << i <<'\n';
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
