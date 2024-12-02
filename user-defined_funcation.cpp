16904eae9af2d485c041b07eeed1cd4d0c8b8567
Tawmeen Hasan Hasib <tawmeenhasanalhasib259@gmail.com>
2024-12-03 01:54:40 +0600

    To run this code

---
+++ user-defined_funcation.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void happybirthday(string name , int age);
+
+int main(){
+
+// funcation = a block of reuseable code 
+
+std:: string name = "Tawmeen";
+int age =20;
+
+
+happybirthday(name,age);
+
+    return 0;
+
+}
+
+void happybirthday(std:: string name , int age ){
+
+    cout <<"Happy birthday to you " <<name <<'\n';
+    cout <<"Happy birthday to you " <<name <<'\n';
+    cout <<"Happy birthday Dear " <<name <<'\n';
+    cout <<"You are " <<age <<"years old" <<'\n';
+
+
+
+
+
+}
